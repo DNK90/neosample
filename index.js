d96df3c1e195ab2313e6844176754509ad2955f1
Kien Dang <kien.15@gmail.com>
2018-05-21 15:20:14 +0700

    add code

---
+++ index.js
@@ -0,0 +1,71 @@
+const neonjs = require('@cityofzion/neon-js');
+
+let PRIVATE_KEY = "KxDgvEKzgSBPPfuVfw67oPQBSjidEiqTHURKSDL1R7yGaGYAeYnr";
+let account = new neonjs.wallet.Account(PRIVATE_KEY);
+let fromAddrScriptHash = neonjs.wallet.getScriptHashFromAddress(account.address);
+let net = "http://localhost:5000";
+let contract = "86d58778c8d29e03182f38369f0d97782d303cc0";
+let gas = 1;
+
+let rpcUrl = {
+    local: [
+    	"http://localhost:8081",
+    	"http://localhost:30333",
+    	"http://localhost:30334",
+    	"http://localhost:30335",
+    	"http://localhost:30336"
+    ]
+};
+
+function signTx(tx, publicKey) {
+    return Promise.resolve(neonjs.tx.signTransaction(tx, account.privateKey))
+}
+
+let tx_output = new neonjs.tx.TransactionOutput({
+    assetId: neonjs.CONST.ASSET_ID.GAS,
+    scriptHash: fromAddrScriptHash,
+    value: new neonjs.u.Fixed8(gas)
+});
+
+let intents = [tx_output];
+
+function sendrawtransaction(idx) {
+	if (idx < rpcUrl.local.length) {
+		neonjs.api.doInvoke({
+		    url: rpcUrl.local[idx],
+		    net: net,
+		    intents: intents,
+		    script: neonjs.default.create.script({
+		        scriptHash: contract,
+		        args: [
+		            neonjs.u.str2hexstring("add"),
+		            neonjs.u.int2hex(1),
+		            neonjs.u.int2hex(2)
+		        ]
+		    }),
+		    address: account.address,
+		    publicKey: account.publicKey,
+		    signingFunction: signTx,
+		    privateKey: account.privateKey,
+		    gas: gas,
+		    override: {
+		       attributes: [
+		           {
+		               data: neonjs.u.hash160(neonjs.wallet.getVerificationScriptFromPublicKey(account.publicKey)),
+		               usage: neonjs.tx.TxAttrUsage.Script
+		           }
+		       ]
+		    }
+		}).then(function(result) {
+
+			console.log(JSON.stringify(result));
+			if (result.response.result == true) {
+				return;
+			} 
+			sendrawtransaction(++idx);
+		    
+		});
+	}
+}
+
+sendrawtransaction(0);
